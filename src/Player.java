97e9540a9f87f6a7ceca9faa9d9e8f17efbdf99d
Chobilet <afmartinez1215@gmail.com>
2021-01-26 15:34:07 -0800

    Created constructor for player, sets default stats in constructor

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,11 @@
 public class Player extends Entity {
-
-
-
+    //Starting player stats
+    final Double START_ATTACK = 2.5;
+    final Double START_HEALTH = 20.0;
+    //constructor
+    public Player(){
+        super();
+        setAttack(START_ATTACK);
+        setHealth(START_HEALTH);
+    }
 }
