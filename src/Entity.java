6256b75936a892bb787faade591e5771351263ac
Chobilet <afmartinez1215@gmail.com>
2021-03-28 16:57:52 -0700

    Entities now have x and y coords

--- src/Entity.java
+++ src/Entity.java
@@ -1,9 +1,13 @@
+import java.util.zip.DeflaterInputStream;
+
 public abstract class Entity {
 
     //Every entity should have these
     protected Double health;
     protected Double attack;
     protected Boolean alive;
+    protected Integer xCord;
+    protected Integer yCord;
 
     //constructor
     public Entity(){
@@ -15,19 +19,35 @@ public abstract class Entity {
     public void setHealth(Double healthModify){
         this.health = healthModify;
     }
-    public void setAttack(double attackModify){
+    public void setAttack(Double attackModify){
         this.attack = attackModify;
     }
     public void setAlive(Boolean status){
         this.alive = status;
     }
 
+    public void setXCord(Integer xCord){
+        this.xCord = xCord;
+    }
+
+    public void setYCord(Integer yCord){
+        this.yCord = yCord;
+    }
+
     //Getters
-    public Double gethealth(){
+    public Double getHealth(){
         return health;
     }
     public Double getAttack(){
         return attack;
+    }
+
+    public Integer getxCord(){
+        return xCord;
+    }
+
+    public Integer getyCord(){
+        return yCord;
     }
 
 }
