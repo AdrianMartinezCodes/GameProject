8dc28b4ce0df5de541be8654a00ea656d3079bf8
Chobilet <afmartinez1215@gmail.com>
2021-03-28 16:57:11 -0700

    Collisions now extends entity class

--- src/Collisions.java
+++ src/Collisions.java
@@ -6,7 +6,7 @@
 
 import java.util.Random;
 
-public class Collisions {
+public class Collisions extends Entity{
     /*
     * Initiates EntityOne attack on EntityTwo
     * Chance to miss based on normal distribution(0,1). Attack is intiated
@@ -14,19 +14,18 @@ public class Collisions {
     * TODO: add a way to modify miss chance(maybe luck stat or base off of defence).
      */
 
-    Random rand = new Random();
+    private static final Random rand = new Random();
 
-    public boolean InitiateAttack(Entity EntityOne, Entity EntityTwo){
-        Double entityOneMiss = rand.nextGaussian();
-        Double CHANCE_TO_MISS = 0.5;
+    public boolean initiateAttack(Entity EntityTwo){
+        double entityOneMiss = rand.nextGaussian();
+        double CHANCE_TO_MISS = 0.5;
 
         if(entityOneMiss < CHANCE_TO_MISS){ return false; }
         else{
-            Double updatedMobHealth = EntityTwo.gethealth() - EntityOne.getAttack();
+            Double updatedMobHealth = EntityTwo.getHealth() - getAttack();
             EntityTwo.setHealth(updatedMobHealth);
             return true;
         }
-
     }
 
 }
