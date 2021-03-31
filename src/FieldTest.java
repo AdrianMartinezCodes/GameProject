3e15a367f42c291654718a81a96cd89cea64a58d
Chobilet <afmartinez1215@gmail.com>
2021-03-30 22:34:13 -0700

    Additional tests for Field class

--- src/FieldTest.java
+++ src/FieldTest.java
@@ -3,11 +3,44 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FieldTest {
-
+    Field field;
+    Player player;
+    Mob mob;
     @Before
     public void setup(){
-        Field field = new Field();
-        Player player = new Player();
-        Mob mob = new Mob();
+        field = new Field();
+        player = new Player();
+        mob = new Mob();
     }
+
+    @Test
+    public void addPlayer(){
+        assertTrue(field.generateEntity(player));
+    }
+
+    @Test
+    public void addMob(){
+        assertTrue(field.generateEntity(mob));
+    }
+
+    @Test
+    public void checkPopulationOne(){
+        field.generateEntity(player);
+        assertEquals(1,field.population);
+    }
+
+    @Test
+    public void fieldFull(){
+        Field smallField = new Field(2,2);
+        for(int i = 0; i < field.maxPopulation; i++){
+            field.generateEntity(new Mob());
+        }
+        assertTrue(field.fieldIsFull());
+    }
+
+    @Test
+    public void fieldNotFull(){
+        assertFalse(field.fieldIsFull());
+    }
+
 }
